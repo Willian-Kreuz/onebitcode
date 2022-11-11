1ac975cbb25eecf4d376d0bc2fefe6bf1d467393
Willian Nathan Kreuz <112355980+Willian-Kreuz@users.noreply.github.com>
2022-11-10 22:17:05 -0300

    javascript

--- JavaScript/Aula 16 - While/while.js
+++
@@ -1,9 +0,0 @@
-let velocidade = 80
-
-while (velocidade > 0){
-    alert("O carro está a " + velocidade + " km/h")
-    velocidade -=20
-    alert("Diminuindo 20km/h")
-}
-
-alert("O carro parou!")
