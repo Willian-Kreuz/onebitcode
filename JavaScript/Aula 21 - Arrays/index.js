5cb730dc5a952283ac08dd261a25778b7ab1fa0e
Willian Nathan Kreuz <112355980+Willian-Kreuz@users.noreply.github.com>
2022-11-16 22:13:12 -0300

    limpeza

--- JavaScript/Aula 21 - Arrays/index.js
+++
@@ -1,9 +0,0 @@
-const listaDeCompras = ["açucar"]
-console.log (listaDeCompras)
-
-listaDeCompras[0] = "Arroz"
-listaDeCompras[1] = "Feijão"
-listaDeCompras[2] = 7
-listaDeCompras[5] = "batata"
-
-console.log(listaDeCompras)
