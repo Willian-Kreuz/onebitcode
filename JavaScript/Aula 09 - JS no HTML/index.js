794468b64e1c71b99dd91846dc29095e6334c976
Willian Nathan Kreuz <112355980+Willian-Kreuz@users.noreply.github.com>
2023-01-23 12:42:27 -0300

    limpeza

--- JavaScript/Aula 09 - JS no HTML/index.js
+++
@@ -1,6 +0,0 @@
-let name = "Willian"
-let x = 5
-
-console.log(name)
-console.log(x)
-
--- JavaScript/Aula 20 - Exercício tabuada e palidromos/Tabuada/index.js
+++
@@ -1,9 +0,0 @@
-const number = parseFloat(prompt("Digite um número: "))
-
-let tabuada = ""
-
-for(let calculo = 0; calculo <=20; calculo++){
-    tabuada += " -> " + number + " x " + calculo + " = " + (number*calculo + "\n")
-}
-
-alert(tabuada)
