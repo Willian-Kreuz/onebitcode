4b3201453fa8387432ad6903610027c8f5b63938
Willian Nathan Kreuz <willian.kreuz1998@gmail.com>
2023-02-13 13:15:52 -0300

    Exercício + aula

--- JavaScript/exercício 9/calculadoraGeometrica.js
+++ JavaScript/exercício 9/calculadoraGeometrica.js
@@ -10,6 +10,20 @@ function retangulo(base,altura){
     return areaRetangulo
 }
 
+function quadrado(lado){
+    const areaQuadrado = lado*lado
+    return areaQuadrado
+}
+
+function trapezio(baseMaior,baseMenor,altura){
+    const areaTrapezio = (baseMaior + baseMenor) * (altura/2)
+    return areaTrapezio
+}
+
+function circulo(raio){
+    const areaCirculo = 3.14 * (raio*raio)
+    return areaCirculo
+}
 do{
     option = prompt("Bem-vindo a calculadora geométrica! \n" +
     "\nEscolha a opção desejada: "+
@@ -32,8 +46,19 @@ do{
             alert("A área do retângulo é: " + retangulo(baseRetangulo,alturaRetangulo))
             break
         case "3":
+            let ladoQuadrado = parseFloat(prompt("Digite o lado do quadrado: "))
+            alert("A área do quadrado é: " + quadrado(ladoQuadrado))
+            break
         case "4":
+            let baseMaiorTrapezio = parseFloat(prompt("Digite a base maior do trapezio: "))
+            let baseMenorTrazpezio = parseFloat(prompt("Digite a base menor do trapezio: "))
+            let alturaTrapezio = parseFloat(prompt("Digite a altura do trapézio: "))
+            alert("A área do trapézio é: " + trapezio(baseMaiorTrapezio, baseMenorTrazpezio,alturaTrapezio))
+            break
         case "5":
+            let raioCirculo = parseFloat(prompt("Digite o raio do círculo: "))
+            alert("A área do círculo é: " + circulo(raioCirculo))
+            break
         case "6":
             alert("Saindo do sistema...")
             break
