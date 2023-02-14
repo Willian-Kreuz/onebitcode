b68e96e7517d26b32b12c2f89903984714046065
Willian Nathan Kreuz <willian.kreuz1998@gmail.com>
2023-02-14 14:31:33 -0300

    Funções anonimas e recursivas

---
+++ JavaScript/Aula - funções anônimas/anonima.js
@@ -0,0 +1,13 @@
+function somar (a,b){
+    return a + b
+}
+
+const operacao = somar
+
+console.log(operacao(3,5))
+
+const subtracao = function (a,b){
+    return a - b
+}
+
+console.log(subtracao(6,2))
