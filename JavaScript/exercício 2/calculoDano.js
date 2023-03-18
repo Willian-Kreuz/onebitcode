674c4ae37f809880a077eb24671f360a4ded720f
Willian Nathan Kreuz <112355980+Willian-Kreuz@users.noreply.github.com>
2023-03-18 07:29:09 -0300

    Refazendo exercícios para relembrar conteúdo

--- JavaScript/exercício 2/calculoDano.js
+++ JavaScript/exercício 2/calculoDano.js
@@ -1,26 +1,25 @@
-const attackersName = prompt("Digite o nome do primeiro personagem: ")
-let attackPower = parseFloat(prompt("Digite o poder de ataque desse personagem: "))
-const defendersName = prompt("Digite o nome do segundo personagem: ")
-let healthPoints = parseFloat(prompt("Digite quanto de vida tem o segundo personagem: "))
-let defendersPower = parseFloat(prompt("Digite o poder de defesa do segundo personagem: "))
-const shield = prompt("Informe se o segundo personagem possui escudo: (s/n)")
+const attackPerson = prompt("Digite o nome do personagem de ataque: ")
+const powerAttack = parseFloat(prompt("Digite o poder de ataque: "))
+const defensePerson = prompt("Digite o nome do defensor: ")
+let life = parseFloat(prompt("Digite os pontos de vida do defensor: "))
+let powerDefense = parseFloat(prompt("Digite o poder de defesa do persongaem: "))
+const shield = prompt("O personagem possui escudo (s/n)?")
 
-let damage = 0
-let life = healthPoints
-if(attackPower > defendersPower && shield === 'n'){
-    damage = attackPower - defendersPower
-}else if(attackPower > defendersPower && shield === 's'){
-    damage = (attackPower - defendersPower)/2
-}else if(attackPower <= defendersPower){
-    damage = 0
+let demage = 0
+
+if(powerAttack > powerDefense && shield === 'n'){
+    demage = powerAttack - powerDefense
+}else if (powerAttack > powerDefense && shield === 's'){
+    demage = (powerAttack - powerDefense) / 2
+}else{
+    demage = 0
 }
 
-alert("Informações dos personagens: \n" +
-"\nNome do atacante: " + attackersName +
-"\nPoder de ataque: " + attackPower +
-"\n\nNome do defensor: " + defendersName +
-"\nVida total: " + healthPoints +
-"\nPoder de defesa: " + defendersPower +
-"\nPossui escudo: " + shield +
-"\nDano sofrido: " + damage +
-"\n\nVida atualizada após o dano: " + (life-damage))
+alert("Informações dos personagens: \n" + 
+"\nAtacante: " + attackPerson +
+"\nPoder de ataque: " + powerAttack +
+"\n\nDefensor: " + defensePerson +
+"\nVida: " + life +
+"\nPoder de defesa: " + powerDefense +
+"\nEscudo: " + shield +
+"\nVida do defensor após o ataque: " + (life-demage))
