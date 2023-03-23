f268f3829e36a56e5c677678591879df29bfd879
Willian Nathan Kreuz <112355980+Willian-Kreuz@users.noreply.github.com>
2023-03-23 06:39:16 -0300

    refazendo exercícios

--- JavaScript/exercício 9/calculadoraGeometrica.js
+++ JavaScript/exercício 9/calculadoraGeometrica.js
@@ -1,32 +1,51 @@
 let option = ""
 
-function triangulo(base,altura){
-    const areaTriangulo = (base*altura) / 2
-    return areaTriangulo
+function base(){
+    return prompt("Digite o valor da base: ")
 }
 
-function retangulo(base,altura){
-    const areaRetangulo = base*altura
-    return areaRetangulo
+function altura(){
+    return prompt("Digite o valor da altura: ")
 }
 
-function quadrado(lado){
-    const areaQuadrado = lado*lado
-    return areaQuadrado
+function lado(){
+    return prompt("Digite o valor do lado: ")
 }
 
-function trapezio(baseMaior,baseMenor,altura){
-    const areaTrapezio = (baseMaior + baseMenor) * (altura/2)
-    return areaTrapezio
+function baseMaior(){
+    return prompt("Digite o valor da base maior: ")
 }
 
-function circulo(raio){
-    const areaCirculo = 3.14 * (raio*raio)
-    return areaCirculo
+function baseMenor(){
+    return prompt("Digite o valor da base menor: ")
+}
+
+function raio(){
+    return prompt("Digite o valor do raio")
+}
+
+function areaTriangulo(base, altura){
+    return (base*altura) / 2
+}
+
+function areaRetangulo(base, altura){
+    return base * altura
+}
+
+function areaQuadrado (lado){
+    return lado * lado
+}
+
+function areaTrapezio (baseMaior, baseMenor, altura){
+    return (baseMaior + baseMenor) * altura / 2
+}
+
+function areaCirculo(raio){
+    return 3.14 * (raio*raio)
 }
 do{
-    option = prompt("Bem-vindo a calculadora geométrica! \n" +
-    "\nEscolha a opção desejada: "+
+    option = prompt("Bem-vindo a calculadora geométrica!\n\n"+
+    "Escolha a opção desejada:"+
     "\n1 - Calcular a área do triângulo"+
     "\n2 - Calcular a área do retângulo"+
     "\n3 - Calcular a área do quadrado"+
@@ -35,32 +54,36 @@ do{
     "\n6 - Sair")
 
     switch(option){
-        case "1":
-            let baseTriangulo = parseFloat(prompt("Digite a base do triângulo: "))
-            let alturaTriangulo = parseFloat(prompt("Digite a altura do triângulo: "))
-            alert("A área do triângulo é: " + triangulo(baseTriangulo,alturaTriangulo))
+        case '1':
+            let baseTriangulo = parseFloat(base())
+            let alturaTriangulo = parseFloat(altura())
+            alert("A Área do triângulo é: " + areaTriangulo(baseTriangulo,alturaTriangulo))
             break
-        case "2":
-            let baseRetangulo = parseFloat(prompt("Digite a base do retângulo: "))
-            let alturaRetangulo = parseFloat(prompt("Digite a altura do retângulo: "))
-            alert("A área do retângulo é: " + retangulo(baseRetangulo,alturaRetangulo))
+        case '2':
+            let baseRetangulo = parseFloat(base())
+            let alturaRetangulo = parseFloat(altura())
+            alert("A área do retângulo é: " + areaRetangulo(baseRetangulo, alturaRetangulo))
             break
-        case "3":
-            let ladoQuadrado = parseFloat(prompt("Digite o lado do quadrado: "))
-            alert("A área do quadrado é: " + quadrado(ladoQuadrado))
+        case '3':
+            let ladoQuadrado = parseFloat(lado())
+            alert("A área do quadrado é: " + areaQuadrado(ladoQuadrado))
             break
-        case "4":
-            let baseMaiorTrapezio = parseFloat(prompt("Digite a base maior do trapezio: "))
-            let baseMenorTrazpezio = parseFloat(prompt("Digite a base menor do trapezio: "))
-            let alturaTrapezio = parseFloat(prompt("Digite a altura do trapézio: "))
-            alert("A área do trapézio é: " + trapezio(baseMaiorTrapezio, baseMenorTrazpezio,alturaTrapezio))
+        case '4':
+            let baseMa = parseFloat(baseMaior())
+            let baseMe = parseFloat(baseMenor())
+            let alturaTrapezio = parseFloat(altura())
+            alert("A área do trapézio é: " + areaTrapezio(baseMa, baseMe, alturaTrapezio))
             break
-        case "5":
-            let raioCirculo = parseFloat(prompt("Digite o raio do círculo: "))
-            alert("A área do círculo é: " + circulo(raioCirculo))
+        case '5':
+            let valorRaio = parseFloat(raio())
+            alert("A área do círculo é: " + areaCirculo(valorRaio))
             break
-        case "6":
+        break
+        case '6':
             alert("Saindo do sistema...")
             break
+        default:
+            alert("Opção inválida")
     }
-}while (option != "6")
+
+}while(option != '6')
